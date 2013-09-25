76e43564e971b7a96562b4e81219b05b9c6f6558
Dan Ring <dfring@gmail.com>
2013-09-25 09:58:58 -0700

    Fix exception pattern matching for internal unicode strings

--- src/ggrc/models/exceptions.py
+++ src/ggrc/models/exceptions.py
@@ -11,18 +11,11 @@ def translate_message(exception):
   Translates db exceptions to something a user can understand.
   """
   message = exception.message
-  print exception
-  print dir(exception)
-  print type(exception)
-  print exception.message
-  print exception.orig
   if isinstance(exception, IntegrityError):
-    print "INSIDE"
     # TODO: Handle not null, foreign key errors, uniqueness errors with compound keys
-    duplicate_entry_pattern = re.compile(r'\(1062, "(Duplicate entry \'[^\']*\')')
+    duplicate_entry_pattern = re.compile(r'\(1062, u?"(Duplicate entry \'[^\']*\')')
     matches = duplicate_entry_pattern.search(message)
     if matches:
-      print "Yup", matches
       return matches.group(1)
     else:
       return message
